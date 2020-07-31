9c180edca66474156bb815f5c41f45883507b1cc
Derek <chenjunqi07@icloud.com>
2020-07-31 11:43:42 +0800

    update

--- WebImageDemo/ContentView.swift
+++ WebImageDemo/ContentView.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+private let url = URL(string: "https://github.com/xiaoyouxinqing/PostDemo/raw/master/PostDemo/Resources/9dc7402fgy1gavd39ba15j20j60nywii.jpg")!
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView{
+            List{
+                NavigationLink(
+                    destination: SimpleExample(url: url)){
+                    Text("Simple Example")
+                }
+                NavigationLink(
+                    destination: WebImageExample()){
+                    Text("Web Image Example")
+                }
+            }
+            .navigationTitle("Title")
+        }
     }
 }
 
--- WebImageDemo/SimpleExample.swift
+++ WebImageDemo/SimpleExample.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct SimpleExample: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    let url: URL?
+    
+    @State private var data: Data?
+    
+    private var image: UIImage? {
+        if let data = data { return UIImage(data: data)}
+        return nil
     }
-}
-
-struct SimpleExample_Previews: PreviewProvider {
-    static var previews: some View {
-        SimpleExample()
+    
+    var body: some View {
+        let image = self.image
+        Group{
+            if image != nil {
+                Image(uiImage: image!)
+                    .resizable()
+                    .scaledToFill()
+            } else {
+                Color.gray
+            }
+        }
+        .frame(height: 600)
+        .clipped()
+        .onAppear {
+            if let url = self.url, self.data == nil {
+                DispatchQueue.global().async {//子线程
+                    let data = try? Data(contentsOf: url)
+                    DispatchQueue.main.async {//主线程
+                        self.data = data
+                    }
+                }
+            }
+        }
     }
 }
