89b74c8d95122942f4173df686e9eab37d99ccee
Derek <chenjunqi07@icloud.com>
2020-07-31 11:26:08 +0800

    update

---
+++ WebImageDemo/SimpleExample.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleExample.swift
+//  WebImageDemo
+//
+//  Created by Derek Chan on 2020/7/31.
+//
+
+import SwiftUI
+
+struct SimpleExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleExample_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleExample()
+    }
+}
---
+++ WebImageDemo/WebImageExample.swift
@@ -0,0 +1,20 @@
+//
+//  WebImageExample.swift
+//  WebImageDemo
+//
+//  Created by Derek Chan on 2020/7/31.
+//
+
+import SwiftUI
+
+struct WebImageExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebImageExample_Previews: PreviewProvider {
+    static var previews: some View {
+        WebImageExample()
+    }
+}
